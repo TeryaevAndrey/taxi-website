2ce1edf8ae22f481806fe08e047ba8ede04c0ba0
Teryaev Andrey <teryaev.2004@mail.ru>
2022-06-23 17:44:17 +0300

    add-main-settings-webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+const pages = ['index'];
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/scripts/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new miniCssExtractPlugin(),
+        new copyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/images'),
+                    to: 'images'
+                }
+            ]
+        })
+    ].concat(pages.map(page => new htmlWebpackPlugin({
+        template: path.resolve(__dirname, `src/${page}.html`),
+        filename: `${page}.html`
+    }))),
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        }
+    }
+}
